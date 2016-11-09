c15ef54c76f1f15e5270cd0be54735a2df5f061b
basso414f <km.414f@gmail.com>
2016-11-10 01:23:15 +0900

    環境依存を削除、画像ファイルのみを走査
    static/pictureのフォルダすべてを操作するが、.gitkeepは入れておきたいので
    画像っぽいファイル名を選んで一覧にするように変更
    あとは環境依存を削除して、cloneした場所でどこでも動くようにしました
    
    ただしまだ動かし方がわかっていない

--- slick/index.py
+++ slick/index.py
@@ -1,6 +1,7 @@
 from bottle import static_file, get, route, run, template
 import os
 import json
+import re
 
 @route('/static/<filepath:path>')
 def static(filepath):
@@ -12,11 +13,13 @@ def index():
 
 @get('/img')
 def getImg():
-    directory = '/Users/bear/work/slideshow/slick/static/picture'
+    directory = os.path.join(os.getcwd() + '/static/picture')
     files = os.listdir(directory)
     result = { 'img': [] }
+    matcher = re.compile('.+(\.jpg|\.JPG|\.JPEG|\.jpeg)')
     for f in files:
-        result['img'].append({ 'fname': f })
+        if matcher.match(f):
+            result['img'].append({ 'fname': f })
     return json.dumps(result)
 
 run(host='localhost', port=8080)
